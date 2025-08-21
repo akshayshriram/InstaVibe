608fed6b135df1109ef9432f5718dcc52a39a1f3
Akshay <akshayshriram999@gmail.com>
2025-08-21 13:53:09 +0530

    Optimize the Post Feed by FlatList

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,13 @@
 import { Loader } from "@/components/Loader";
-import NoPostsFound from "@/components/NoPostsFound";
 import Post from "@/components/Post";
-import Story from "@/components/Story";
-import { STORIES } from "@/constants/mock-data";
+import StoriesSection from "@/components/StoriesSection";
 import { COLORS } from "@/constants/theme";
 import { api } from "@/convex/_generated/api";
 import { styles } from "@/styles/feed.styles";
 import { useAuth } from "@clerk/clerk-expo";
 import { Ionicons } from "@expo/vector-icons";
 import { useQuery } from "convex/react";
-import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
 export default function Index() {
   const { signOut } = useAuth();
 
@@ -29,25 +27,13 @@ export default function Index() {
         </TouchableOpacity>
       </View>
 
-      {/* Story Section */}
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          style={styles.storiesContainer}
-        >
-          {STORIES.map((story) => (
-            <Story key={story.id} story={story} />
-          ))}
-        </ScrollView>
-
-        {/* Feed Section */}
-        {posts.length === 0 ? (
-          <NoPostsFound />
-        ) : (
-          posts.map((post) => <Post key={post._id} post={post} />)
-        )}
-      </ScrollView>
+      <FlatList
+        data={posts}
+        renderItem={({ item }) => <Post post={item} />}
+        keyExtractor={(item) => item._id}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={<StoriesSection />}
+      />
     </View>
   );
 }
---
+++ components/StoriesSection.tsx
@@ -0,0 +1,19 @@
+import { STORIES } from "@/constants/mock-data";
+import { styles } from "@/styles/feed.styles";
+import React from "react";
+import { ScrollView } from "react-native";
+import Story from "./Story";
+
+export default function StoriesSection() {
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      style={styles.storiesContainer}
+    >
+      {STORIES.map((story) => (
+        <Story key={story.id} story={story} />
+      ))}
+    </ScrollView>
+  );
+}
