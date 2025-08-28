66bde3f46406dd4c4f1be7c5ab0e6e739ef585f4
Akshay <akshayshriram999@gmail.com>
2025-08-28 13:59:35 +0530

    Notification Init

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -45,7 +45,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="notifications"
+        name="notification"
         options={{
           tabBarIcon: ({ size, color }) => (
             <Ionicons name="heart" size={size} color={color} />
--- app/(tabs)/notification.tsx
+++ app/(tabs)/notification.tsx
@@ -1,10 +1,32 @@
-import { View, Text } from "react-native";
+import { Loader } from "@/components/Loader";
+import NoPostsFound from "@/components/NoPostsFound";
+import Notification from "@/components/Notification";
+import { api } from "@/convex/_generated/api";
+import { styles } from "@/styles/notifications.styles";
+import { useQuery } from "convex/react";
 import React from "react";
+import { FlatList, Text, View } from "react-native";
 
 export default function notification() {
+  const notifications = useQuery(api.notifications.getNotifications);
+
+  if (notification === undefined) return <Loader />;
+  if (notification.length === 0)
+    return <NoPostsFound content="Notifications" />;
+
   return (
-    <View>
-      <Text>notification screen</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Notifications</Text>
+
+        <FlatList
+          data={notifications}
+          renderItem={({ item }) => <Notification notification={item} />}
+          keyExtractor={(item) => item._id}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.listContainer}
+        />
+      </View>
     </View>
   );
 }
