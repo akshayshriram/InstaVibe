a9236bf4ed604d82b103464510a5d8eb217b1538
Akshay <akshayshriram999@gmail.com>
2025-09-01 14:02:00 +0530

    Profile Screen Init...

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,85 @@
-import { Text, View } from "react-native";
+import { Loader } from "@/components/Loader";
+import { COLORS } from "@/constants/theme";
+import { api } from "@/convex/_generated/api";
+import { Doc } from "@/convex/_generated/dataModel";
+import { styles } from "@/styles/profile.styles";
+import { useAuth } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
+import { useMutation, useQuery } from "convex/react";
+import { Image } from "expo-image";
+import { useState } from "react";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 
 export default function Profile() {
+  const { signOut, userId } = useAuth();
+  const [isEditModalVisible, setIsEditModalVisible] = useState(false);
+
+  const currentUser = useQuery(
+    api.users.getUserByClerkID,
+    userId
+      ? {
+          clerkId: userId,
+        }
+      : "skip"
+  );
+
+  const [editdProfile, setEditdProfile] = useState({
+    fullname: currentUser?.fullname || "",
+    bio: currentUser?.bio || "",
+  });
+
+  const [selectedPost, setSelectedPost] = useState<Doc<"posts"> | null>(null);
+  const posts = useQuery(api.posts.getPostByUser, {});
+
+  const updateProfile = useMutation(api.users.updateProfile);
+
+  const handleSaveProfle = async () => {};
+
+  if (!currentUser || posts === undefined) return <Loader />;
+
   return (
-    <View>
-      <Text>Profile Screen</Text>
+    <View style={styles.container}>
+      {/* Header */}
+      <View style={styles.header}>
+        <View style={styles.headerLeft}>
+          <Text style={styles.username}>{currentUser.username}</Text>
+        </View>
+        <View style={styles.headerRight}>
+          <TouchableOpacity style={styles.headerIcon} onPress={() => signOut()}>
+            <Ionicons name="log-out-outline" size={24} color={COLORS.white} />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.profileInfo}>
+          <View style={styles.avatarAndStats}></View>
+          <View>
+            <Image
+              source={currentUser.image}
+              style={styles.avatar}
+              contentFit="cover"
+              transition={200}
+            />
+            <View style={styles.statsContainer}>
+              <View style={styles.statItem}>
+                <Text style={styles.statNumber}>{currentUser.posts}</Text>
+                <Text style={styles.statLabel}>Posts</Text>
+              </View>
+              <View style={styles.statItem}>
+                <Text style={styles.statNumber}>{currentUser.followers}</Text>
+                <Text style={styles.statLabel}>Followers</Text>
+              </View>
+              <View style={styles.statItem}>
+                <Text style={styles.statNumber}>{currentUser.following}</Text>
+                <Text style={styles.statLabel}>Following</Text>
+              </View>
+            </View>
+          </View>
+          <Text style={styles.name}>{currentUser.fullname}</Text>
+          {currentUser.bio && <Text style={styles.bio}>{currentUser.bio}</Text>}
+        </View>
+      </ScrollView>
     </View>
   );
 }
